1c5108449dec1c41677f0f137b3ed0c2ae43dde8
Emma <emma.sahagun@iest.edu.mx>
2026-09-09 17:32:20 -0600

    Initial Commit

---
+++ PrimerParcialEmmaSahagun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrimerParcialEmmaSahagun
+//
+//  Created by win603 on 09/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrimerParcialEmmaSahagun/PrimerParcialEmmaSahagunApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimerParcialEmmaSahagunApp.swift
+//  PrimerParcialEmmaSahagun
+//
+//  Created by win603 on 09/09/26.
+//
+
+import SwiftUI
+
+@main
+struct PrimerParcialEmmaSahagunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
